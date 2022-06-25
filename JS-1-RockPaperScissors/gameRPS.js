a4995cb87a887ae2db7feda451f8c2316397bbd0
jlsolito <36279354+jlsolito@users.noreply.github.com>
2022-06-25 02:34:15 -0400

    Revamped functions and added more
    - created get() and set() functions for player and computer inputs
    - created separate functions to convert index to text results
    - add game() function to tie all functions together and play a full round with updated scoring
    - game() also runs tests
    - have tests for up to showing a round winner
    - need to test and fix scoreboard

--- JS-1-RockPaperScissors/gameRPS.js
+++ JS-1-RockPaperScissors/gameRPS.js
@@ -1,19 +1,78 @@
-// computerPlay
+// global variables
+let pScore = 0;
+let cScore = 0;
+let pRounds = [];
+let cRounds = [];
+const hands = ['rock', 'paper', 'scissors']
+const winner = ['It\'s a draw!', 'Player wins!', 'Computer wins!']
+const outcomes = [
+    [0, 1, 2],
+    [2, 0 ,1],
+    [1, 2, 0]
+];
+
+// convert number to rock, paper, or scissors
+function getHand(num){
+    return hands[num];
+}
+
+// computerPlay() to pick the Computer's hand randomly
 function computerPlay() {
-    const num = Math.floor(Math.random() * 3);
-    let roll;
-    switch(num) {
-        case 0:
-            roll = 'rock';
-            break;
+    return Math.floor(Math.random() * 3);
+}
+
+function playerPlay() {
+    const selectBox = document.getElementById('player');
+    const index = selectBox.selectedIndex;
+    return Number(selectBox.options[index].value);
+}
+
+// playRound() to evaluate winner of a round
+function playRound(playerSelection, computerSelection){
+    return outcomes[playerSelection][computerSelection];
+}
+
+function getWinner(index) {
+    return winner[index];
+}
+
+function scoreboard(result){
+    
+    switch(result){
         case 1:
-            roll = 'paper';
+            pScore++;
+            pRounds.push('Win');
+            cRounds.push('Loss');
             break;
         case 2:
-            roll = 'scissors';
+            cScore++;
+            cRounds.push('Win');
+            pRounds.push('Loss');
             break;
         default:
-            break; 
+            cRounds.push('Draw');
+            pRounds.push('Draw');
+            break;
     }
-    return roll;
+}
+
+// game() to play a round and keep score
+function game(){
+    cp001(); // test if computerPlay returns a number
+    cp002(); // test if playerPlay() returns a number
+    cp003(); // test if computerPlay() outputs anything that is not rock, paper, or scissors
+    cp004(); // test if playerPlay() outputs anything that is not rock, paper, or scissors
+    rnd005(); // test if playRound() returns number
+    win006(); // test if winner is selected
+
+    let pSelect = playerPlay();
+    let cSelect = computerPlay();
+    let compare = playRound(pSelect, cSelect);
+    scoreboard(compare);
+    
+    
+    
+    console.log(`Player chooses ${getHand(pSelect)} and Computer chooses ${getHand(cSelect)}. ${getWinner(compare)}`);
+    console.log(`Score:\n\tPlayer has ${pScore} wins\n\tComputer has ${cScore} wins.`)
+    console.log(`Player: ${pRounds}\nComputer: ${cRounds}`);
 }
--- JS-1-RockPaperScissors/test.js
+++ JS-1-RockPaperScissors/test.js
@@ -1,23 +1,71 @@
-// test if computerPlay() returns a string
+// test if computerPlay() returns a number
 function cp001(){
     const roll = computerPlay();
-    if(typeof roll === 'string') {
-        console.log(`cp001()\nPASS: roll is a string\nroll = ${roll}.`);
+    if(typeof roll === 'number') {
+        console.log(`cp001()\n\tPASS: Computer roll is a number\n\troll = ${roll}.`);
     }
     else {
-        console.log(`cp001()\nFAIL: roll is not a string! Type is ${typeof roll}.`);
+        console.log(`cp001()\n\tFAIL: Computer roll is not a number! Type is ${typeof roll}.`);
+    }
+}
+
+// test if playerPlay() returns a number
+function cp002(){
+    const roll = playerPlay();
+    if(typeof roll === 'number') {
+        console.log(`cp002()\n\tPASS: Player roll is a number\n\troll = ${roll}.`);
+    }
+    else {
+        console.log(`cp002()\n\tFAIL: Player roll is not a number! Type is ${typeof roll}.`);
     }
 }
 
 // test if computerPlay() outputs anything that is not rock, paper, or scissors
-function cp002(){
+function cp003(){
     let i;
+    let roll;
     for(i=0; i < 100; i++){
-        roll = computerPlay();
+        roll = getHand(computerPlay());
         if(roll !== 'rock' && roll !== 'paper' && roll !== 'scissors'){
-            console.log(`cp002()\nFAIL: computerPlay() rolled ${roll} after ${i+1} iterations.`);
+            console.log(`cp003()\n\tFAIL: computerPlay() rolled ${roll} after ${i+1} iterations.`);
             break;
         }
     }
-    if(i == 100) console.log(`cp002()\nPASS: computerPlay() plays fair and square.`)
+    if(i == 100) console.log(`cp003()\n\tPASS: computerPlay() plays fair and square.\n\tComputer hand = ${roll}`)
+}
+
+// test if playerPlay() outputs anything that is not rock, paper, or scissors
+function cp004(){
+    let i;
+    let roll;
+    for(i=0; i < 100; i++){
+        roll = getHand(playerPlay());
+        if(roll !== 'rock' && roll !== 'paper' && roll !== 'scissors'){
+            console.log(`cp004()\n\tFAIL: playerPlay() rolled ${roll} after ${i+1} iterations.`);
+            break;
+        }
+    }
+    if(i == 100) console.log(`cp004()\n\tPASS: playerPlay() plays fair and square.\n\tPlayer hand = ${roll}`)
+}
+
+// test if playRound() returns number
+function rnd005(){
+    const round = playRound(playerPlay(), computerPlay());
+    if(typeof round === 'number'){
+        console.log(`rnd005()\n\tPASS: playRound() returns an index for winner[]`);
+    }
+    else {
+        console.log(`rnd005()\n\tFAIL: playRound() returns a ${typeof round}`);
+    }
+}
+
+// test if winner is selected
+function win006(){
+    const win = getWinner(playRound(playerPlay(), computerPlay()));
+    if(win == 'It\'s a draw!' || win == 'Player wins!' || win == 'Computer wins!'){
+        console.log(`win006()\n\tPASS: A winner has been chosen. ${win}`);
+    }
+    else {
+        console.log(`win006()\n\tFAIL: Unknown outcome: ${win}`);
+    }
 }
